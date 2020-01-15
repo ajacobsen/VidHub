78b6544e636a87290de545e7c223d1c76d769169
atze <atze.danjo@gmail.com>
2020-01-15 20:33:03 +0100

    Update streamer urls

--- streamer/urls.py
+++ streamer/urls.py
@@ -4,16 +4,16 @@ from . import views
 
 app_name = 'streamer'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('watch/<watch_id>', views.video, name='watch'),
-    path('channel/<channel_id>', views.channel, name='channel'),
-    path('upload/', views.uploadVideo, name='upload'),
-    path('history/', views.history, name='history'),
-    path('subscribe/', views.subscribe, name='subscribe'),
-    path('like/', views.like, name='like'),
-    path('dislike/', views.dislike, name='dislike'),
-    path('comment/', views.comment, name='comment'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/<watch_id>', views.editVideo, name='edit_video'),
-    path('dashboard/delete/', views.deleteVideo, name='delete_video'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('watch/<watch_id>', views.VideoView.as_view(), name='watch'),
+    path('channel/<channel_id>', views.ChannelView.as_view(), name='channel'),
+    path('upload/', views.UploadVideoView.as_view(), name='upload'),
+    path('history/', views.HistoryView.as_view(), name='history'),
+    path('subscribe/', views.SubscribeView.as_view(), name='subscribe'),
+    path('like/', views.LikeView.as_view(), name='like'),
+    path('dislike/', views.DislikeView.as_view(), name='dislike'),
+    path('comment/', views.CommentView.as_view(), name='comment'),
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('dashboard/<watch_id>', views.EditVideoView.as_view(), name='edit_video'),
+    path('dashboard/delete/', views.DeleteVideoView.as_view(), name='delete_video'),
 ]
